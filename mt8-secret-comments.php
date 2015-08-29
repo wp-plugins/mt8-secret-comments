43b9362cf23c077e57b7486d1a4058d0d735a2f4
mt8.biz <mt8.biz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-29 22:55:40 +0000

    tagging version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/mt8-secret-comments/tags/1.0.1@1233894 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mt8-secret-comments.php
@@ -0,0 +1,136 @@
+<?php
+/*
+	Plugin Name: MT8 Secret Comments
+	Plugin URI: https://github.com/mt8/mt8-secret-comments
+	Description: Write a comment visible only to admin.
+	Author: mt8.biz
+	Version: 1.0
+	Author URI: http://mt8.biz
+	Domain Path: /languages
+	Text Domain: mt8-secret-comments
+*/	
+
+	$mt8_sc = new Mt8_Secret_Comments();
+	$mt8_sc->register_hooks();
+
+	class Mt8_Secret_Comments {
+
+		const TEXT_DOMAIN = 'mt8-secret-comments';
+		const META_KEY = 'mt8_secret_comments';
+		
+		public function __construct() {
+			
+		}
+
+		public function register_hooks() {
+			
+			add_action( 'plugins_loaded', array( &$this, 'plugins_loaded' ) );
+			
+			add_action( 'admin_init', array( &$this, 'admin_init' ) );
+			add_filter( 'comment_save_pre', array( &$this, 'comment_save_pre' ) );			
+			add_filter( 'comment_form_field_comment', array( &$this, 'comment_form_field_comment' ) );
+			
+			add_action( 'comment_post', array( &$this, 'comment_post' ) );
+			add_filter( 'get_comment_text', array( &$this, 'get_comment_text' ), 10, 3 );
+			
+			add_filter( 'manage_edit-comments_columns', array( &$this, 'manage_edit_comments_columns' ) );			
+			add_filter( 'manage_comments_custom_column', array( &$this, 'manage_comments_custom_column'), 10, 2 );		
+
+		}
+		
+		public function plugins_loaded() {
+			
+			load_plugin_textdomain( self::TEXT_DOMAIN, false, dirname( plugin_basename( __FILE__ ) ).'/languages' );
+			
+		}
+		
+		public function admin_init() {
+
+			add_meta_box( 'mt8_sc', __( 'Show only admin.', self::TEXT_DOMAIN ), array( &$this, 'add_meta_box_comment' ), 'comment', 'normal' );
+						
+		}
+		
+		public function add_meta_box_comment() {
+			
+			global $comment_id;
+			$secret = get_comment_meta( $comment_id, self::META_KEY, true );
+			$checked = checked( $secret, 1, false );
+			$comment_field = 
+			'
+			<label for="'.self::META_KEY.'">
+				<input name="'.self::META_KEY.'" id="'.self::META_KEY.'" type="checkbox" value="1" ' . $checked . '>
+			</label>
+			';
+			echo $comment_field;
+		}
+		
+		public function comment_save_pre( $comment_content ) {
+			
+			global $comment_id;
+			$this->comment_post( $comment_id );
+			
+			return $comment_content;
+		}
+
+		public function comment_form_field_comment( $comment_field ) {
+
+			$comment_field .= 
+			'
+			<label for="'.self::META_KEY.'">
+				<input name="'.self::META_KEY.'" id="'.self::META_KEY.'" type="checkbox" value="1" >
+				' . __( 'Show only admin.', self::TEXT_DOMAIN ) . '
+			</label>
+			';
+			return $comment_field;
+
+		}
+    
+		public function comment_post( $comment_id ) {
+			
+			if ( isset( $_POST[self::META_KEY] ) ) {
+				$mt8_sc = ( in_array( $_POST[ self::META_KEY ], array( '0', '1' ) ) ) ? $_POST[ self::META_KEY ] : '0';
+			} else {
+				$mt8_sc = '0';
+			}
+			update_comment_meta( $comment_id, self::META_KEY, $mt8_sc );
+			
+		}
+		
+		public function get_comment_text( $comment_content, $comment, $args ) {
+
+			if ( !is_singular() ) {
+				return $comment_content;
+			}
+			
+			if ( $this->is_secret( $comment->comment_ID ) && ! current_user_can( 'administrator' ) ) {
+				return __( 'Only administrators can see this comment.', self::TEXT_DOMAIN );
+			} else {
+				return $comment_content;
+			}
+			
+		}
+
+		public function manage_edit_comments_columns( $columns ) {
+			
+			$columns['mt8_sc'] = __( 'Show only admin.', self::TEXT_DOMAIN );
+			return $columns;
+			
+		}
+
+		public function manage_comments_custom_column( $column, $comment_id ) {
+
+			if ( 'mt8_sc' == $column ) {
+				if ( $this->is_secret($comment_id) ) {
+					echo '*';
+				}
+			}
+
+		}
+		
+		public function is_secret( $comment_id ) {
+			
+			return ( get_comment_meta( $comment_id, self::META_KEY, true ) == '1' );
+			
+		}
+		
+	}
---
+++ readme.txt
@@ -0,0 +1,51 @@
+=== Mt8 Secret Comments ===
+Contributors: mt8.biz
+Donate link: http://mt8.biz
+Tags: comment,secret
+Requires at least: 4.2
+Tested up to: 4.3
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Write a comment visible only to admin.
+
+== Description ==
+
+Mt8 Secret Commentsは、管理者にしか見れないコメントを書き込みできるようになるプラグインです。
+
+
+== Installation ==
+
+1. Upload `plugin-name.zip` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+1. Latest information
+
+== Frequently Asked Questions ==
+
+Still nothing.
+
+== Upgrade Notice ==
+
+= 1.0 =
+
+* first release
+
+= 1.0.1 =
+
+* Tested up to: 4.3
+* fix plugin tags in readme.txt
+
+== Changelog ==
+
+= 1.0 =
+
+* 初版リリース
+
+= 1.0.1 =
+
+* 4.3でテスト
+* readme.txt
